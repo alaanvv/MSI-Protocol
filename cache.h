aa66a91d4e5fcba4b220b881027dfcfc631c5a6a
alaanvv <valealan84@gmail.com>
2026-06-07 10:21:26 -0300

    refactor: tag_lookup

--- cache.h
+++ cache.h
@@ -6,36 +6,42 @@
 
 // ---
 
-u8 cache_rd(Cache* cache, u16 addr) {
-  u16  index = addr / CACHE_LINE_SIZE % CACHE_LINE_COUNT;
+u8 tag_lookup(Cache* cache, u16 addr, u16* tag_out, u16* index_out) {
   u16    tag = addr / CACHE_LINE_SIZE / CACHE_LINE_COUNT;
-
-  Line line = cache->lines[index];
-  u8    hit = line.valid && tag == line.tag;
-
-  printf("--  READING  --\n");
-  printf("Address  0x%04x (%d)\n", addr, addr);
-  printf("├── Tag  0x%04x (%d)\n", tag, tag);
-  printf("└── Idx  0x%04x (%d)\n", index, index);
-  print_cache_line(line, index);
-
-  if (hit) printf("Cache hit\n");
-  else     printf("Cache miss\n");
-  return hit;
-}
-
-u8 cache_wr(Cache* cache, u16 addr) {
   u16  index = addr / CACHE_LINE_SIZE % CACHE_LINE_COUNT;
-  u16    tag = addr / CACHE_LINE_SIZE / CACHE_LINE_COUNT;
-
   Line* line = &cache->lines[index];
-  u8    hit = line->valid && tag == line->tag;
 
-  printf("--  WRITING  --\n");
+  if (tag_out)   *tag_out   = tag;
+  if (index_out) *index_out = index;
+
+  u8 hit = line->valid && tag == line->tag;
+
   printf("Address  0x%04x (%d)\n", addr, addr);
   printf("├── Tag  0x%04x (%d)\n", tag, tag);
   printf("└── Idx  0x%04x (%d)\n", index, index);
   print_cache_line(*line, index);
+
+  if (hit) printf("Cache hit\n");
+  else     printf("Cache miss\n");
+
+  return hit;
+}
+
+u8 cache_rd(Cache* cache, u16 addr) {
+  printf("--  READING  --\n");
+
+  u8 hit = tag_lookup(cache, addr, NULL, NULL);
+
+  return hit;
+}
+
+u8 cache_wr(Cache* cache, u16 addr) {
+  printf("--  WRITING  --\n");
+
+  u16 tag, index;
+  u8 hit = tag_lookup(cache, addr, &tag, &index);
+
+  Line* line = &cache->lines[index];
 
   if (hit) {
     line->tag   = tag;
@@ -44,8 +50,6 @@ u8 cache_wr(Cache* cache, u16 addr) {
     print_cache_line(*line, index);
   }
 
-  if (hit) printf("Cache hit\n");
-  else     printf("Cache miss\n");
   return hit;
 }
 
