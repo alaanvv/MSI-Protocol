117156e42acaa7e8625ea81909247946b9acde41
alaanvv <valealan84@gmail.com>
2026-06-06 21:37:15 -0300

    feat: ui adjusts

--- ui.h
+++ ui.h
@@ -4,13 +4,13 @@
 #include "core.h"
 
 void print_cache_header() {
-  printf("         Valid | Dirty | Tag    | Data\n");
+  printf("            V | D | Tag    | Data\n");
 }
 
 void print_cache_line(Line line, u8 index) {
-  printf(" %03d -> ", index);
-  printf(" %d     |", line.valid);
-  printf(" %d     |", line.dirty);
+  printf(" %03d   ->  ", index);
+  printf(" %d |", line.valid);
+  printf(" %d |", line.dirty);
   printf(" 0x%04x | ", line.tag);
   for (u8 i = 0; i < CACHE_LINE_SIZE; i++) printf("0x%x ", line.bytes[i]);
   printf("\n");
