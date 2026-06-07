0634ab69bfaa638aaa8198463a2e4f47130f6f5b
alaanvv <valealan84@gmail.com>
2026-06-07 10:29:46 -0300

    feat: main

--- main.c
+++ main.c
@@ -1,10 +1,27 @@
 #include "cache.h"
 
 int main() {
-  Cache c = { 0 };
+  Cache cores[4] = { 0 };
 
-  cache_rd(&c, 1);
-  cache_wr(&c, 1);
+  for (u8 c = 0; c < 4; c++)
+    for (u32 l = 0; l < CACHE_LINE_COUNT; l++)
+      cores[c].lines[l].state = 'I';
+
+  FILE* file = fopen("TRACE.txt", "r");
+  c8 buffer[64];
+
+  u8 i = 0;
+  while (fgets(buffer, 64, file) && i++ < 10) {
+    u8 core_id;
+    char op;
+    u16 addr;
+
+    sscanf(buffer, "CORE %hhd %c%*s %hx", &core_id, &op, &addr);
+
+    Cache* cache = &cores[core_id];
+    if (op == 'R') cache_rd(cache, addr);
+    else           cache_wr(cache, addr);
+  }
 
   return 0;
 }
